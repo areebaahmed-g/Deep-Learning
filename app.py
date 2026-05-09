66d068b5b5fdcfe5496e83dedc15278b90007d2a
Areeba Mansoor <areebamansoor500@gmail.com>
2026-05-09 11:20:27 +0500

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ model = load_model("diabetes_ann_model.h5")
 
 # Load scaler
 with open("scaler(2).pkl", "rb") as f:
-    scaler = pickle.load(f)
+    scaler(2) = pickle.load(f)
 
 st.title("Diabetes Prediction App")
 
